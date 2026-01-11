5308b00d5ec34ca56740b6c77f6a2cfaac49aebf
Patryk Golonka <patryk.golonka@proton.me>
2026-01-11 17:35:58 +0100

    EEPROM Read/Write on rp pico

--- src/boards/rp2040/eeprom-board.c
+++ src/boards/rp2040/eeprom-board.c
@@ -7,13 +7,25 @@
 
 #include "utilities.h"
 #include "eeprom-board.h"
+#include "hardware/flash.h"
+#include "hardware/sync.h"
+
+#define EEPROM_READ_START (XIP_BASE + PICO_FLASH_SIZE_BYTES - 2*FLASH_BLOCK_SIZE)
+#define EEPROM_WRITE_START (PICO_FLASH_SIZE_BYTES - 2*FLASH_BLOCK_SIZE)
 
 uint8_t EepromMcuReadBuffer( uint16_t addr, uint8_t *buffer, uint16_t size )
 {
-    return FAIL;
+    
+    uint8_t* flash = (uint8_t*) EEPROM_READ_START;
+    memcpy1(buffer, flash, size);
+
+    return SUCCESS;
 }
 
 uint8_t EepromMcuWriteBuffer( uint16_t addr, uint8_t *buffer, uint16_t size )
 {
-    return FAIL;
+    uint32_t ints = save_and_disable_interrupts();
+    flash_range_program(EEPROM_WRITE_START + addr, buffer, size);
+    restore_interrupts(ints);
+    return SUCCESS;
 }
